b7a98b42323b25c756c436bd7aa6c6827d11fcd3
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-02-14 16:57:47 +0100

    :construction: Add loading page design

---
+++ scripts/loading_page.js
@@ -0,0 +1,12 @@
+class Loading{
+    constructor(){
+        this.$loading_page = document.querySelector('.js-loading_page_container')
+        
+        this.init()
+    }
+    init(){
+
+    }
+}
+
+const loading = new Loading()
