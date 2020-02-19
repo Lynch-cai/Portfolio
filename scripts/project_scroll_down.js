c85ae38b76cecc284529e735100e4c993082cb69
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-02-19 19:33:35 +0100

    :construction: Get scroll information & add new background img for parallax

--- scripts/project_scroll_down.js
+++ scripts/project_scroll_down.js
@@ -1,10 +1,24 @@
-class Scroll_down{
-    constructor(){
-        
-        this.init()
-    }
-    init(){
+let scroll_direction = '' // top, down, right, left
+let scroll_power = ''
+$(function() {
+    var loghandle = function(event, delta) {
+        var o = ''
+        if (event.deltaY > 0)
+            o += 'top';
+        else if (event.deltaY < 0)
+            o += 'down';
 
-    }
-}
-const scroll_down = new Scroll_down() 
+        if (event.deltaX > 0)
+            o += 'right';
+        else if (event.deltaX < 0)
+            o += 'left';
+        scroll_direction = o
+        scroll_power = event.deltaFactor
+        console.log(scroll_direction);
+        console.log(scroll_power);
+    };
+    $(document)
+        .mousewheel(function(event, delta) {
+            loghandle(event, delta);
+        });
+});
