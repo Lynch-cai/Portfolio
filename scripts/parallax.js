e6ad9909539b3c01f2ffb07ed3579686ee767a82
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-02-19 23:41:40 +0100

    :zap: :tada: Improve, fix parallax & View Project & Comment

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -3,35 +3,93 @@ class Parallax{
         this.sizes = {}
         this.sizes.width = window.innerWidth
         this.sizes.height = window.innerHeight
+        this.ratioX = ''
+        this.ratioY = ''
+        this.translateX = ''
+        this.translateY = ''
+        this.parallax_power = '-10'
+        this.scale_min = 1.15
+        this.scale = this.scale_min
+        this.scale_max = 1.45
+        this.scale_power = 0.08
         this.resize()
         
         this.$project_background = document.querySelector('.js-project_background')
-        this.$global_cursor = document.querySelector('.js-global_cursor')
         this.parallax()
+
+        this.$view_project = document.querySelector('.js-project_view_button')
+        this.view_project()
     }
+
+    // RESIZE window to prevent user to change size
     resize(){
         window.addEventListener(
             'resize',
             () => {
                 this.sizes.width = window.innerWidth
                 this.sizes.height = window.innerHeight
-                
             }
         )
     }
+
+    // GET user cursor position & calculate the parallax movement
     parallax(){
-        this.$global_cursor.addEventListener(
+        document.addEventListener(
             'mousemove',
             (_event)=>{
-                const ratioX = _event.clientX / this.sizes.width - 0.5 // give value between -0.5 & 0.5
-                const ratioY = _event.clientY / this.sizes.height - 0.5
-                const translateX = ratioX * -10
-                const translateY = ratioY * -10
-                this.$project_background.style.transform = `translate(${translateX}%, ${translateY}%) scale(1.1)`
+                this.ratioX = _event.clientX / this.sizes.width - 0.5 // give value between -0.5 & 0.5
+                this.ratioY = _event.clientY / this.sizes.height - 0.5
+                this.translateX = this.ratioX * this.parallax_power
+                this.translateY = this.ratioY * this.parallax_power
+            }
+        )
+    }
+
+    // APPLY the parallax even if user hover "view project"
+    view_project(){
+
+        // DECLARE previewTime (usefull to get the time between each frame) & view_project_active (usefull to change scale)
+        let previousTime = Date.now()
+        let view_project_active = false
+
+        // FUNCTION each frame (animation)
+        const animation_loop = ()=>{
+            requestAnimationFrame(animation_loop)
+
+            // GET time for each frame
+            const time = Date.now()
+            const delta = time - previousTime
+            previousTime = time
+
+            // if user hover "View project", increase scale
+            if (view_project_active && this.scale < this.scale_max){
+                this.scale += this.scale_power * (delta/100)
+            }
+
+            // if user leave hover "View project", decrease scale
+            else if(!view_project_active && this.scale > this.scale_min){
+                this.scale -= this.scale_power * (delta/100)
+            }
+            
+            this.$project_background.style.transform = `translate3d(${this.translateX}%, ${this.translateY}%, 0) scale(${this.scale})`
+        }
+        animation_loop()
+
+        // CHECK if user hover "view project"
+        this.$view_project.addEventListener(
+            'mouseover',
+            ()=>{
+                view_project_active = true
+                this.$project_background.classList.add('active')
+            }
+        )
+        this.$view_project.addEventListener(
+            'mouseout',
+            ()=>{
+                view_project_active = false
+                this.$project_background.classList.remove('active')
             }
         )
     }
 }
 const parallax = new Parallax()
-
-
