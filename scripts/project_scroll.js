72762053e4ad15175e49feffb1ee71f8ac056f68
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-02-21 23:54:58 +0100

    :construction: Change method when user scroll (desc)
    Only change text when user scroll

--- scripts/project_scroll.js
+++ scripts/project_scroll.js
@@ -38,33 +38,41 @@ $(function() {
 class Scroll{
     constructor(){
         this.$projects_container = document.querySelector('.js-projects_container')
-        this.$project_container = document.querySelectorAll('.js-project_container')
+        this.$project_container = this.$projects_container.querySelectorAll('.js-project_container')
         this.$transition_page = this.$projects_container.querySelector('.js-project_transition_page')
+
+
+        this.$project_content = this.$project_container.querySelector('.js-project_content')
+        this.$project_number = this.$project_content.querySelector('.js-project_number') // change number
+        // this.$project_acronym = this.$project_content.querySelector('.js-project_acronym') // delete letter if needed
+        // this.$project_acronym_letter = this.$project_content.querySelectorAll('.js-project_acronym span') // change letter
+        this.$project_link = this.$project_content.querySelector('.js-project_view_link') // change href link value
+        this.$project_background = this.$project_content.querySelector('.js-project_background') // change class to change background
+
+
+
+        js-project_view_link
+
     }
     up(){
         if (project_number <= this.$project_container.length - 1 && project_number > 0) {
             project_number -= 1
-            project_pos -= sizes.height
         }
         else{
             project_number = this.$project_container.length - 1
-            project_pos = sizes.height * (this.$project_container.length - 1)
         }
     }
     down(){
         if (project_number < this.$project_container.length - 1) {
             project_number += 1
-            project_pos += sizes.height
         }
         else{
             project_number = 0
-            project_pos = 0
         }
     }
     scrolling(){
         console.log(project_number);
         console.log(project_pos);
-        this.$project_container[0].style.transform = `translate3d(0, ${project_pos}px, 0)`
     }
     // left(){
     //     if (project_number < this.$project_container.length - 1) {
