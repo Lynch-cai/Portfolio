559312d00cf4c20f1112c30365de7929fed96256
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-03-08 18:38:55 +0100

    :fire: Add comments

--- scripts/image_smooth_scroll.js
+++ scripts/image_smooth_scroll.js
@@ -5,8 +5,6 @@ class SmoothScroll{
         this.init()
     }
     init(){
-
-        
         // Smooth image movement
         const img_scroll = ()=>{
             // GET image height & y pos
@@ -15,26 +13,24 @@ class SmoothScroll{
 
             const height_diff = img_bounding.height - img_container_bounding.height
 
-
             // DETECT if the img is on user screen
             const img_appear = img_container_bounding.y - sizes.height // img appear when this value = 0
             const img_disappear = img_container_bounding.y + img_container_bounding.height // img disappear when this value = 0
-
             if((img_appear <= 0 && img_disappear >= 0) || (img_disappear <= 0 && img_appear >= 0)){
                 const img_diff = Math.abs(img_appear) + Math.abs(img_disappear)
 
+                // Transform translate
                 const img_translate_strength = -0.5+(Math.abs(img_appear)/img_diff)
                 const img_movement = img_translate_strength*height_diff
 
+                // Transform Scale
                 const img_scale_strength = 4 // lower value = higher scale
                 const img_scale = 1+(Math.abs(img_appear)/img_diff)/img_scale_strength
                 
+                // APPLY
                 this.$img.style.transform = `translateY(${img_movement}px) scale(${img_scale})`
-
-                
             }
         }
-
 
 
         // DETECT Scroll
