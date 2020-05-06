82ee4c84da1566003c96eb251328886266a3b52b
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-05-06 17:12:36 +0200

    :tada: Hide cursor on mobile/tablet

--- scripts/mobile_detection.js
+++ scripts/mobile_detection.js
@@ -1,3 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    const cursor = doucment.querySelector('.global_cursor')
+    cursor.style.display = 'none'
     console.log('mobile')
 }
