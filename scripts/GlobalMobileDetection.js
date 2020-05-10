7dadf31f8e32dac496e57791af855396cf195454
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-05-11 00:15:42 +0200

    :tada: Hide cursor when mobile/tablet is detected

---
+++ scripts/GlobalMobileDetection.js
@@ -0,0 +1,9 @@
+// First detection
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    cursor.$cursor_container.style.display = 'none'
+    console.log('mobile')
+}
+// Second detection
+else if (sizes.width <= 768) {
+    cursor.$cursor_container.style.display = 'none'
+}
--- scripts/mobile_detection.js
+++
@@ -1,5 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    const cursor = doucment.querySelector('.global_cursor')
-    cursor.style.display = 'none'
-    console.log('mobile')
-}
